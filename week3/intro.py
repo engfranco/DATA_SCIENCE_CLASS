b47cd6c436ad9842b7272745c8db6a7a1fd8e564
Alexandre R. Franco <eng.franco@gmail.com>
2018-07-31 17:33:25 -0400

    start of week 3

---
+++ week3/intro.py
@@ -0,0 +1,37 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World!")
+
+
+# below is how we caluclate the number of seconds in a year 
+
+# In[2]:
+
+
+365*24*60*60
+
+
+# In[3]:
+
+
+
+
+
+# We can also inlude Latex here 
+# 
+# $\int_0^\infty x^2 dx$
+
+# In[7]:
+
+
+#get_ipython().run_line_magic('matplotlib', 'inline')
+from matplotlib.pyplot import plot
+
+plot([0,1,0,1])
+
+
+# this is a plot using matplotlib
